b560d97b1bb61cba1d173e6b2fdc9d461709db15
DotkeTamir <djdotke@gmail.com>
2016-07-07 20:55:03 +1000

    Project set up files

---
+++ DotkeMusic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DotkeMusic
+//
+//  Created by Dotan Tamir on 1/07/2016.
+//  Copyright © 2016 Dotan Tamir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
