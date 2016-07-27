125bcef4e37a2ad97b29c5e01df0907b5bdcdb84
DotkeTamir <djdotke@gmail.com>
2016-07-27 21:47:27 +1000

    [spike] add animation view, in order for the line player to be independet  from the grid view

--- DotkeMusic/ViewController.swift
+++ DotkeMusic/ViewController.swift
@@ -8,35 +8,22 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var gridVIew: UIView!
     @IBOutlet weak var gridChangeButton: UIButton!
+    @IBOutlet weak var animationView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let value = UIInterfaceOrientation.LandscapeLeft.rawValue
-//        UIDevice.currentDevice().setValue(value, forKey: "orientation")
-//        UIViewController.attemptRotationToDeviceOrientation()
-        
-        let blocks = self.layerToDraw()
-        
-        for i in 0..<blocks.count {
-            let layer = CAShapeLayer()
-            let block = blocks[i]
-            layer.path = UIBezierPath(roundedRect: block.rect, cornerRadius: 0).CGPath
-            layer.backgroundColor = UIColor.blueColor().CGColor
-            layer.lineWidth = 1.0
-            layer.strokeColor = UIColor.redColor().CGColor
-            self.gridVIew.layer.addSublayer(layer)
-        }
         let line = CAShapeLayer()
         line.path = UIBezierPath(roundedRect: CGRect(x: 0 , y: 5, width: self.view.bounds.size.width, height: 1), cornerRadius: 0).CGPath
         line.backgroundColor = UIColor.yellowColor().CGColor
         line.strokeColor = UIColor.whiteColor().CGColor
         line.lineWidth = 1.0
-        self.gridVIew.layer.addSublayer(line)
+        self.animationView.layer.addSublayer(line)
+        
         
         
         let fadeAnimation = CABasicAnimation(keyPath: "transform.translation.y")
         fadeAnimation.fromValue = 1.0
-        fadeAnimation.toValue = self.view.bounds.size.width
+        fadeAnimation.toValue = self.view.frame.size.height
         fadeAnimation.duration = 2
         fadeAnimation.repeatCount = Float(Int.max)
         
@@ -62,36 +49,33 @@ class ViewController: UIViewController {
         return blocks;
     }
     
-//    override func viewWillLayoutSubviews() {
-//        self.gridVIew.layer.sublayers?.removeAll()
-//        let blocks = self.layerToDraw()
-//        
-//        for i in 0..<blocks.count {
-//            let layer = CAShapeLayer()
-//            let block = blocks[i]
-//            layer.path = UIBezierPath(roundedRect: block.rect, cornerRadius: 0).CGPath
-//            layer.backgroundColor = UIColor.blueColor().CGColor
-//            layer.lineWidth = 1.0
-//            layer.strokeColor = UIColor.redColor().CGColor
-//            self.gridVIew.layer.addSublayer(layer)
-//        }
-//
-//    }
+    override func viewWillLayoutSubviews() {
+        self.gridVIew.layer.sublayers?.removeAll()
+        let blocks = self.layerToDraw()
+        
+        for i in 0..<blocks.count {
+            let layer = CAShapeLayer()
+            let block = blocks[i]
+            layer.path = UIBezierPath(roundedRect: block.rect, cornerRadius: 0).CGPath
+            layer.backgroundColor = UIColor.blueColor().CGColor
+            layer.lineWidth = 1.0
+            layer.strokeColor = UIColor.redColor().CGColor
+          self.gridVIew.layer.addSublayer(layer)
+    
+        }
+
+    }
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
     }
     
-//    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-//        return UIInterfaceOrientationMask.LandscapeLeft
-//    }
-//    
     @IBAction func gridChangeTapped(sender: AnyObject) {
         beatGrid /= 2;
+        
+        if(beatGrid == 1/64){
+            beatGrid = 1
+        }
         self.gridChangeButton.setTitle(String(beatGrid), forState: UIControlState.Normal)
         view.layoutIfNeeded()
     }
-//    
-//    override func shouldAutorotate() -> Bool {
-//        return true
-//    }
 }
