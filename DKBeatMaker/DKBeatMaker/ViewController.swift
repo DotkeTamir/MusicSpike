c8637076bdce393c4ab0ee11023c9677ddb22c7a
Dotan Tamir <djdotke@gmail.com>
2016-08-18 10:08:26 +1000

    [setup] set up DKBeatMaker project, and ensuring both ipad and iphone are running in landscape mode

---
+++ DKBeatMaker/DKBeatMaker/ViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.yellowColor()
+//        let value = UIInterfaceOrientation.LandscapeLeft.rawValue
+//        UIDevice.currentDevice().setValue(value, forKey: "orientation")
+        
+  }
+    
+//    override func shouldAutorotate() -> Bool {
+//        return false
+//    }
+//    
+//    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+//        return UIInterfaceOrientationMask.Landscape
+//    }
+}
+
