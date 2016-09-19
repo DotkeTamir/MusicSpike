db083d4f857631abe1c8c63f36a5d3512c569463
Dotan Tamir <djdotke@gmail.com>
2016-09-20 06:28:36 +1000

    [DKBeatMaker] add action outlets for the ADSR

--- DKBeatMaker/DKBeatMaker/Instruments/SynthViewController.swift
+++ DKBeatMaker/DKBeatMaker/Instruments/SynthViewController.swift
@@ -3,4 +3,15 @@ import UIKit
 
 class SynthViewController : UIViewController {
     
+    @IBAction func attackSliderChanged(sender: AnyObject) {
+    }
+    @IBAction func sastainSLiderChange(sender: AnyObject) {
+    }
+    @IBAction func releaseSliderChanged(sender: AnyObject) {
+    }
+    @IBAction func decaySliderChanged(sender: AnyObject) {
+    }
 }
+
+
+
